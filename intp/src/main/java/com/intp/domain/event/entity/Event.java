a5756983ef6ee1a1fade68d4458cdfdf4625937d
Jang HeeJu <93791124+hwnooy@users.noreply.github.com>
2024-01-02 19:22:59 +0900

    [FEATURE/#4] : event entity 작성 및 연관관계 작성

--- intp/src/main/java/com/intp/domain/event/entity/Event.java
+++ intp/src/main/java/com/intp/domain/event/entity/Event.java
@@ -1,2 +1,36 @@
-package com.intp.domain.event.entity;public class Event {
+package com.intp.domain.event.entity;
+
+import com.intp.domain.friend.entity.Friend;
+import com.intp.domain.user.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="event_id")
+    private LocalDateTime event_id;
+
+    @Column(nullable = false)
+    private String event_content;
+
+    @Column(nullable = false)
+    private int price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="friend_id", nullable = false)
+    private Friend friend;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 }
---
+++ intp/src/main/java/com/intp/domain/friend/entity/Friend.java
@@ -0,0 +1,4 @@
+package com.intp.domain.friend.entity;
+
+public class Friend {
+}
