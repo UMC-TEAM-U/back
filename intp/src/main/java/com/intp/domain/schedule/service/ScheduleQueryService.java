f6b389cf4d4c3f99ee128b373fefe7e694a4307f
Parkseojin <seojin5565@gmail.com>
2024-01-03 01:44:48 +0900

    [feat] : 월별 조회완 #18

--- intp/src/main/java/com/intp/domain/schedule/service/ScheduleQueryService.java
+++ intp/src/main/java/com/intp/domain/schedule/service/ScheduleQueryService.java
@@ -1,6 +1,11 @@
 package com.intp.domain.schedule.service;
 
+import com.intp.domain.schedule.dto.ScheduleMonthlyResponseDTO;
+
+import java.util.List;
+
 public interface ScheduleQueryService {
 
-//    public void registerSched
+    public List<ScheduleMonthlyResponseDTO> getMonthlySchedules(Integer targetYear, Integer targetMonth );
+
 }
